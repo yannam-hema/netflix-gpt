d52f3dd0f7d04fa3bb90dfe29ee8278a1036673e
yannam-hema <yannamhema30@gmail.com>
2024-11-08 23:24:08 +0530

    Added Movielist in GptSearch Page

--- src/Utils/constants.js
+++ src/Utils/constants.js
@@ -1,12 +1,12 @@
  export const BackGround_Image="https://assets.nflxext.com/ffe/siteui/vlv3/47c2bc92-5a2a-4f33-8f91-4314e9e62ef1/web/IN-en-20240916-TRIFECTA-perspective_72df5d07-cf3f-4530-9afd-8f1d92d7f1a8_large.jpg";
  export const Netflix_LOGO="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
- export const API_OPTIONS= {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzODA5MjhiMDk2YTQ5N2Y4NWQ1MGM0NWJhYWMzNGUwOCIsIm5iZiI6MTcyNzU5ODM1Ny45MDMzMjQsInN1YiI6IjY2ZjhlYzc4N2YxM2I3YjEyYWEyMzliZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EPgxQ2QaenAv4gYle7C9Zt1ih48HpVHfCFlFLedelDo'
-    }
-  };
+ export const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${process.env.REACT_APP_TMDB_API_KEY}`
+  }
+};
 
 export const IMG_CDN_URL="https://image.tmdb.org/t/p/w500/"
 
@@ -33,16 +33,4 @@ export const LANGUAGES=[
   }
 ]
 
-
-
-
-
-
-
-
-
-
-
-
-
-export const open_api_key=process.env.REACT_APP_OPENAI_KEY;
+export const open_api_key = process.env.REACT_APP_OPENAI_KEY;
